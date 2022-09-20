ce897980ed6bc12ab59d95a671ee29a96b80a734
saff-it <saff_it@ymail.com>
2022-09-20 12:53:40 +0200

    path customization working

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -16,6 +16,7 @@ class PostController extends Controller
     public function index()
     {
         $posts = Post::all();
+        
         return view('admin.posts.index', compact('posts'));
     }
 
